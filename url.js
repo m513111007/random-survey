f861730e2c34b73778e33ebc4ccb6eb3356ae9a2
m513111007 <148835506+m513111007@users.noreply.github.com>
2023-10-24 11:16:08 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,6 +9,6 @@
 
 const urls = [
 	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/S9mzi4o63jinsKhf9',
+	'https://forms.gle/dszzeoRYdXmDjTT28'
 ];
